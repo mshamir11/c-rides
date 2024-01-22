179648ebfbbbc3dc1f529bb915dbbf0a4ab36c2a
mshamir11 <mshamir11@gmail.com>
2024-01-22 10:52:09 +0530

    Added a test button from material UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "@material-tailwind/react";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="div">hello world</div>
+			<Button>Button</Button>
 		</main>
 	);
 }
