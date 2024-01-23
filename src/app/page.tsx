12f41ec31fabd147a442fa2e4d4f408dfea2a0f5
mshamir11 <mshamir11@gmail.com>
2024-01-23 10:44:39 +0530

    Cleaned up the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-
-import Image from "next/image";
-import { Button } from "@material-tailwind/react";
-
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Button>Button</Button>
-		</main>
-	);
+  return <div>Home Page</div>;
 }
