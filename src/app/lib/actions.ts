05a4cfda2248465fca5b4feceaeac0c31cefddc0
mshamir11 <mshamir11@gmail.com>
2024-01-24 10:57:23 +0530

    Added useForm hooks to login page

---
+++ src/app/lib/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  console.log("TODO: implement authenticate");
+}
---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log("TODO: Implement authentication logic here");
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/((?!learning).*)"],
+};
