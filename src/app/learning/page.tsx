9e710320498e1fe8550ca0a0f6b054b4e35c17c8
mshamir11 <mshamir11@gmail.com>
2024-01-30 09:17:06 +0530

    Added box shadow related items

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -139,6 +139,18 @@ export default function Learning() {
       </div>
       <hr />
       <br />
+      <div className="p-4">
+        <h1 className="text-xl font-bold mb-4 ">Shadow </h1>
+        <div className="h-40 flex space-x-5 justify-center items-center bg-gray-200 ">
+          <div className="h-32 shadow-md w-32 bg-slate-200 shadow-red-500/50">
+            01
+          </div>
+          <div className="h-32 w-32 bg-green-500">02</div>
+          <div className="h-32 w-32 bg-purple-500">03</div>
+        </div>
+      </div>
+      <hr />
+      <br />
     </div>
   );
 }
