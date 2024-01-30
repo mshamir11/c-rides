ce4710917db3cedeb71c8150b3d39e6d8102fce7
mshamir11 <mshamir11@gmail.com>
2024-01-30 17:31:02 +0530

    Added table

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -151,6 +151,44 @@ export default function Learning() {
       </div>
       <hr />
       <br />
+      <div className="p-4">
+        <h1 className="text-xl font-bold mb-4 ">Filters </h1>
+        <div className="h-40 flex space-x-5 justify-center items-center  ">
+          <div className="h-32 shadow-md w-32 bg-slate-200 shadow-red-500/50">
+            01
+          </div>
+          <div className="h-32 w-32 hover:blur-sm bg-green-500">
+            Hover over me
+          </div>
+          <div className="drop-shadow-2xl h-32 w-32 brightness-1 bg-purple-500">
+            03
+          </div>
+        </div>
+      </div>
+      <hr />
+      <br />
+      <table className="border-collapse border border-slate-400 ...">
+        <thead>
+          <tr>
+            <th className="border border-slate-300 ...">State</th>
+            <th className="border border-slate-300 ...">City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-slate-300 ...">Indiana</td>
+            <td className="border border-slate-300 ...">Indianapolis</td>
+          </tr>
+          <tr>
+            <td className="border border-slate-300 ...">Ohio</td>
+            <td className="border border-slate-300 ...">Columbus</td>
+          </tr>
+          <tr>
+            <td className="border border-slate-300 ...">Michigan</td>
+            <td className="border border-slate-300 ...">Detroit</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
