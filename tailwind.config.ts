fda3772acf2f967bf936919ebc940e8996e0d1a9
mshamir11 <mshamir11@gmail.com>
2024-02-01 09:54:36 +0530

    Getting rid of material UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
 
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
