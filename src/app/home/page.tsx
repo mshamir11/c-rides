505a2b04c751fd5b60bc0f79a06587fde56c1d13
mshamir11 <mshamir11@gmail.com>
2024-02-01 09:24:33 +0530

    Added home page UI with Create ride, Join ride and Show my rides CTA

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,34 @@
 import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
+  if (session) {
+    return getLandingPageForRegisteredUsers();
+  } else {
+    redirect("api/auth/signIn");
+  }
 }
+
+const getLandingPageForRegisteredUsers = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col container mx:auto justify-center my-4">
+        <h1 className="text-2xl font-bold text-center">Community Rides</h1>
+        <br />
+        <hr />
+        <div className="flex h-16 justify-center  items-center space-x-4">
+          <button className="flex p-4 w-32 h-10 border-2 border-black justify-center items-center rounded-md">
+            Create Ride
+          </button>
+          <button className="flex p-4 w-32 h-10 border-2 border-black justify-center items-center rounded-md">
+            Join Ride
+          </button>
+          <button className="flex p-4 w-40 h-10 border-2 border-black justify-center items-center rounded-md">
+            Show my rides
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
