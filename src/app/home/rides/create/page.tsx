7cf3b9a0ed9105ccc74b9aa46aa1f049b69d1b30
mshamir11 <mshamir11@gmail.com>
2024-02-03 09:41:47 +0530

    Added mock form action to create ride form

--- src/app/home/rides/create/page.tsx
+++ src/app/home/rides/create/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { createRide } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
+
 export default function CreateRidePage() {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createRide, initialState);
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-black">
       <div className="flex flex-col items-center justify-center">
@@ -6,37 +13,57 @@ export default function CreateRidePage() {
           <h1 className="text-2xl font-bold">Create Ride</h1>
         </div>
         <div className="my-6 mt-12">
-          <form action="" className="flex flex-col space-y-12">
+          <form action={dispatch} className="flex flex-col space-y-12">
             <div className="space-y-4 ">
               <div className="flex justify-between mx-2">
                 <label className="" htmlFor="">
                   Ride Name
                 </label>
-                <input className=" border border-black" type="text" />
+                <input
+                  name="rideName"
+                  className=" border border-black"
+                  type="text"
+                />
               </div>
               <div className="flex space-x-2 justify-between mx-2">
                 <label className="" htmlFor="">
                   Destination Location
                 </label>
-                <input className=" border border-black" type="text" />
+                <input
+                  name="destinationLocation"
+                  className=" border border-black"
+                  type="text"
+                />
               </div>
               <div className="flex justify-between mx-2">
                 <label className="" htmlFor="">
                   Start Location
                 </label>
-                <input className="border border-black" type="text" />
+                <input
+                  name="startLocation"
+                  className="border border-black"
+                  type="text"
+                />
               </div>
               <div className="flex justify-between mx-2">
                 <label className="" htmlFor="">
                   Start Date
                 </label>
-                <input className="border border-black" type="date" />
+                <input
+                  name="startDate"
+                  className="border border-black"
+                  type="date"
+                />
               </div>
               <div className="flex justify-between mx-2">
                 <label className="" htmlFor="">
                   Trip Duration
                 </label>
-                <input className="border border-black" type="text" />
+                <input
+                  name="tripDuration"
+                  className="border border-black"
+                  type="text"
+                />
               </div>
             </div>
             <div className="flex justify-end mr-2">
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -6,3 +6,22 @@ export async function authenticate(
 ) {
   console.log("TODO: implement authenticate");
 }
+
+export type State = {
+  errors?: {
+    rideName?: string[];
+    destinationLocation?: string[];
+    startLocation?: string[];
+    startDate?: string[];
+    tripDuration?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createRide(prevState: State, formData: FormData) {
+  console.log("formData keys:", formData.keys());
+  for (const key of formData.entries()) {
+    console.log(key);
+  }
+  return {};
+}
