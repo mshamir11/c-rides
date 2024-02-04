d169ed6491c5966758f54d6fc2a14cf9a6cbec4a
mshamir11 <mshamir11@gmail.com>
2024-02-04 10:52:24 +0530

    Added RideSchema using zod

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,11 +1,24 @@
 "use server";
+import { z } from "zod";
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData
-) {
-  console.log("TODO: implement authenticate");
-}
+const RideSchema = z.object({
+  id: z.string(),
+  rideName: z.string(),
+  destinationLocation: z.string(),
+  startLocation: z.string(),
+  startDate: z.string(),
+  tripDuration: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  createdBy: z.string(),
+});
+
+const CreateRideSchema = RideSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  createdBy: true,
+});
 
 export type State = {
   errors?: {
@@ -19,9 +32,27 @@ export type State = {
 };
 
 export async function createRide(prevState: State, formData: FormData) {
-  console.log("formData keys:", formData.keys());
-  for (const key of formData.entries()) {
-    console.log(key);
+  console.log("im here");
+  const validatedFields = CreateRideSchema.safeParse({
+    rideName: formData.get("rideName"),
+    destinationLocation: formData.get("destinationLocation"),
+    startLocation: formData.get("startLocation"),
+    startDate: formData.get("startDate"),
+    tripDuration: formData.get("tripDuration"),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    console.log("mshamirt: im inside if");
+    console.log(
+      "mshamirt: validatedFieldsErrors:",
+      validatedFields.error.flatten().fieldErrors
+    );
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Failed to create ride"',
+    };
   }
-  return {};
+
+  return { errors: {}, message: "test" };
 }
